0e6a709776db8296770538b765bc66614815e9ae
Kris <cblair91@gmail.com>
2015-03-17 14:26:33 +0000

    And your stuff for tomorrows edit.

--- test.php
+++ test.php
@@ -1,17 +1,17 @@
- <?php 
+<?php 
+require('phpivotal.php');
 
-require ('phpivotal.php');
-
-//Let's setup some static variables, so we can unit test the basic functions. 
-
-$token = 'Put Pivotal Token Here';
-$projectid = 'Pivotal Project ID';
-$storyid = 'Pivotal Story';
+// Let's setup some static variables, so we can unit test the basic functions. 
+$toke		= 'Put Pivotal Token Here';
+$projecti	= 'Pivotal Project ID';
+$storyid	= 'Pivotal Story';
 
 $pivotal = new phpivotal($token);
-$args = array(
-				'name'			=> 'Story Test',
-				'owned_by_id' 	=> 927981,
-			);
+$args = [
+		'name'			=> 'Story Test',
+		'owned_by_id'	=> 927981,
+];
 $data = json_decode($pivotal->addStory($projectid, $args);
- ?>
+
+// We should do stuff with $data here, let's just debug it :D
+print_r($data);
